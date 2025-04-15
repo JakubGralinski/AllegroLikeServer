c2d9ae100220fea0e3b3432e1c3551e3bc28da69
mlaskowski7 <mtlaskowski7@gmail.com>
2025-04-15 12:53:48 +0200

    init: initalize empty project

---
+++ src/main/java/pl/edu/pjwstk/tpo/allegrolike/allegrolikeserver/AllegroLikeServerApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.pjwstk.tpo.allegrolike.allegrolikeserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllegroLikeServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AllegroLikeServerApplication.class, args);
+    }
+
+}
