67847b9c02cc907d1839cdb26adb103b31993b13
JakubGralinski <jakubm.gralinski@gmail.com>
2025-04-19 09:26:51 +0200

    Added SecurityConfig and JWT

---
+++ src/main/java/pl/edu/pjwstk/tpo/allegrolike/allegrolikeserver/JWT/JwtProvider.java
@@ -0,0 +1,56 @@
+package pl.edu.pjwstk.tpo.allegrolike.allegrolikeserver.JWT;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtProvider {
+
+    private final String jwtSecret = "fraza";
+    private final long jwtExpirationMs = 86400000; // 1 day
+
+    // One key for instance
+    private final Key key = Keys.hmacShaKeyFor(jwtSecret.getBytes(StandardCharsets.UTF_8));
+
+    public String generateToken(Authentication authentication) {
+        Date now = new Date();
+        Date expiry = new Date(now.getTime() + jwtExpirationMs);
+
+        return Jwts.builder()
+                .setSubject(authentication.getName())
+                .setIssuedAt(now)
+                .setExpiration(expiry)
+                .signWith(key, SignatureAlgorithm.HS512)
+                .compact();
+    }
+
+    public String getUsernameFromToken(String token) {
+        Claims claims = Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+
+        return claims.getSubject();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder()
+                    .setSigningKey(key)
+                    .build()
+                    .parseClaimsJws(token);
+            return true;
+        } catch (Exception ex) {
+            return false;
+        }
+    }
+}
