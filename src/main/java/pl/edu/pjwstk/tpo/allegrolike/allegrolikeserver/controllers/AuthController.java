3063e7835e97cccf04c87199d3214ff28da07672
mlaskowski7 <mtlaskowski7@gmail.com>
2025-05-17 15:02:53 +0200

    fix: make registration return 409 conflict when email taken

--- src/main/java/pl/edu/pjwstk/tpo/allegrolike/allegrolikeserver/controllers/AuthController.java
+++ src/main/java/pl/edu/pjwstk/tpo/allegrolike/allegrolikeserver/controllers/AuthController.java
@@ -1,6 +1,7 @@
 package pl.edu.pjwstk.tpo.allegrolike.allegrolikeserver.controllers;
 
 import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -46,7 +47,7 @@ public class AuthController {
         if (registeredResponse.isPresent()) {
             return ResponseEntity.ok(registeredResponse.get());
         } else {
-            return ResponseEntity.badRequest().body("Provided username is already taken");
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("Provided username or email is already taken");
         }
     }
 
--- src/main/java/pl/edu/pjwstk/tpo/allegrolike/allegrolikeserver/repositories/UserRepository.java
+++ src/main/java/pl/edu/pjwstk/tpo/allegrolike/allegrolikeserver/repositories/UserRepository.java
@@ -6,5 +6,8 @@ import pl.edu.pjwstk.tpo.allegrolike.allegrolikeserver.models.User;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
     Optional<User> findByUsername(String username);
+
+    Optional<User> findByEmail(String email);
 }
